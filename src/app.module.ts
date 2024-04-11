c07a48186707494cd3d3f81ff3d7ef46f55709a0
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-04-11 02:42:04 +0200

    project init and file structure setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { TweetsModule } from './tweets/tweets.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthenticationModule, TweetsModule, UserModule],
+  controllers: [AppController, AuthenticationController],
+  providers: [AppService, AuthenticationService],
 })
 export class AppModule {}
